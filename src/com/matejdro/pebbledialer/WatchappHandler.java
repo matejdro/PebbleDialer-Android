627dc678d3ed3620b6d475d05bf13188d4019e4d
Matej <matejdro@gmail.com>
2013-10-18 18:37:44 +0200

    Watchapp update

--- src/com/matejdro/pebbledialer/WatchappHandler.java
+++ src/com/matejdro/pebbledialer/WatchappHandler.java
@@ -15,7 +15,7 @@ import android.content.SharedPreferences.Editor;
 import android.net.Uri;
 
 public class WatchappHandler {
-	public static final int INCLUDED_WATCHFACE = 3;
+	public static final int INCLUDED_WATCHFACE = 4;
 
 	public static boolean isLatest(SharedPreferences settings)
 	{
